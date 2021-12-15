89b98fa76fe80a64244f8203b77c5f45d62a5dde
jeffpriz <jeffpriz@hotmail.com>
2021-12-15 10:26:49 -0600

    Get Wiki for the project

--- src/contributions/wikiAge/wikiAge.tsx
+++ src/contributions/wikiAge/wikiAge.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
 import { showRootComponent } from "../../Common";
 import * as SDK from "azure-devops-extension-sdk";
+import * as API from "azure-devops-extension-api";
 import { CommonServiceIds, IProjectPageService,IGlobalMessagesService, getClient, IProjectInfo } from "azure-devops-extension-api";
 import { GitServiceIds, IVersionControlRepositoryService } from "azure-devops-extension-api/Git/GitServices";
 import { Page } from "azure-devops-ui/Page";
+import { GitRestClient, GitPullRequest, PullRequestStatus, GitPullRequestSearchCriteria, GitBranchStats } from "azure-devops-extension-api/Git";
+import {WikiRestClient, WikiV2} from "azure-devops-extension-api/Wiki";
+
 
 interface IWikiAgeState {
     projectID:string;
     projectName:string;
+    projectWikiID:string;
+    projectWikiName:string;
+    projectWikiRepoID:string;
+    
 }
 
 class WikiAgeContent extends React.Component<{}, IWikiAgeState> {
@@ -23,29 +31,89 @@ class WikiAgeContent extends React.Component<{}, IWikiAgeState> {
 
     public initEmptyState():IWikiAgeState
     {
-        let initState:IWikiAgeState = {projectID:"", projectName:""};
+        let initState:IWikiAgeState = {projectID:"", projectName:"", projectWikiID:"", projectWikiName:"", projectWikiRepoID:""};
         return initState;
     }
 
         //After the Component has successfully loaded and is ready for processing, begin the initialization
     public async componentDidMount() {        
-            await SDK.init();
-            await SDK.ready();
-            SDK.getConfiguration()
-            SDK.getExtensionContext()
-            const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
-            const project = await projectService.getProject();
-            let newState:IWikiAgeState = this.initEmptyState();
-            if(project) {
-                newState.projectID = project.id;
-                newState.projectName = project.name
-                this.setState(newState);                
-            }
-            else {
-                this.toastError("Did not retrieve the project info");
-            }
-            
+        await SDK.init();
+        await SDK.ready();
+        SDK.getConfiguration()
+        SDK.getExtensionContext()
+        const projectService = await SDK.getService<IProjectPageService>(CommonServiceIds.ProjectPageService);
+        const project = await projectService.getProject();
+        let newState:IWikiAgeState = this.initEmptyState();
+        if(project) {
+            newState.projectID = project.id;
+            newState.projectName = project.name
+            this.setState(newState);
+            await this.DoWork();                
         }
+        else {
+            this.toastError("Did not retrieve the project info");
+        }
+        
+    }
+
+    private async DoWork(){
+        let s:IWikiAgeState = this.state;
+        
+        console.log("Do Worlo");
+        try {
+            let w:WikiV2 = await this.FindProjectWiki(s.projectID);
+
+            if(w)
+            {
+                console.log("have a wiki:");
+                s.projectWikiID = w.id;
+                s.projectWikiName = w.name;
+                s.projectWikiRepoID = w.repositoryId;
+
+                this.setState(s);
+
+            }
+        }
+        catch(ex)
+        {
+            this.toastError(JSON.stringify(ex));
+        }
+
+    }
+        
+
+    private async FindProjectWiki(projectID:string):Promise<WikiV2>
+    {
+        return new Promise<WikiV2>(async (resolve,reject) => {
+            let wClient = await this.GetWikiAPIClient();
+            let thisProjectWiki:WikiV2;
+            let success:boolean = false;
+            if(wClient)
+            {
+                let wikiList:WikiV2[] = await wClient.getAllWikis(projectID);
+                if(wikiList)
+                {
+                    if(wikiList.length >0)
+                    {
+                        wikiList.forEach((w)=> {
+                            if(w.projectId == projectID)
+                            {
+                                console.log("found a wiki");
+                                thisProjectWiki = w;
+                                success =true;
+                                resolve(thisProjectWiki);  
+                            }
+                        });
+                    }
+                }
+            }
+            if(!success)
+            { 
+                reject("no project wiki found")
+            }
+        });
+    }
+
     ///Toast Error
     private async toastError(toastText:string)
     {
@@ -57,12 +125,53 @@ class WikiAgeContent extends React.Component<{}, IWikiAgeState> {
         //this.setState({isToastVisible:true, isToastFadingOut:false, exception:toastText})
     }
 
+
+    public async GetGitAPIClient():Promise<GitRestClient>
+    {
+        return new Promise<GitRestClient>(async (resolve,reject) => {
+            try {
+                let c:GitRestClient = API.getClient(GitRestClient);
+                resolve(c);
+            }
+            catch(ex)
+            {
+                this.toastError("Error while attempting to get Git Client");
+                reject(ex); 
+            }
+        });
+    }
+
+    public async GetWikiAPIClient():Promise<WikiRestClient>
+    {
+        return new Promise<WikiRestClient>(async (resolve,reject) => {
+            try {
+                let c:WikiRestClient = API.getClient(WikiRestClient);
+                resolve(c);
+            }
+            catch(ex)
+            {
+                this.toastError("Error while attempting to get Git Client");
+                reject(ex); 
+            }
+        });
+    }
+
+
+
     public render(): JSX.Element {
 
         let projectName = this.state.projectName
+        let wikiName = this.state.projectWikiName
         return (
             <Page className="sample-hub flex-grow">
-                <div>Hello World  - {projectName}</div>              
+                <div>Hello World  - {projectName}</div>             
+                <div>
+                    WikiName - {wikiName}
+                    <br></br>
+                    WikiId - {this.state.projectWikiID}
+                    <br></br>
+                    WikiRepoID - {this.state.projectWikiRepoID}
+                </div> 
             </Page>
         );
     }
